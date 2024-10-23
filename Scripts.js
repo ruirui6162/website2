307084833cb089d660c7429d8a6047d7a91590d0
ruirui <122705553+ruirui6162@users.noreply.github.com>
2024-10-24 01:20:50 +0800

    Add files via upload

---
+++ Scripts.js
@@ -0,0 +1,16 @@
+﻿// 切换侧边栏的显示状态
+function toggleSidebar() {
+    var sidebar = document.getElementById("sidebar");
+    var mainContent = document.getElementById("main-content");
+
+    // 判断侧边栏是否已经展开
+    if (sidebar.style.width === "250px") {
+        // 隐藏侧边栏
+        sidebar.style.width = "0";
+        mainContent.style.marginLeft = "0";
+    } else {
+        // 展开侧边栏
+        sidebar.style.width = "250px";
+        mainContent.style.marginLeft = "250px";
+    }
+}
